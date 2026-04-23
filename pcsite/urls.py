7102405d4989362a9322d4979cac731cc6ab6ded
Larysa2007 <kulik.larisa12@gmail.com>
2026-04-23 23:38:34 +0300

    Full fix

--- pcsite/urls.py
+++ pcsite/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from configurator import views
+from configurator import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('save/', views.save_build, name='save_build'), 
+    path('builds/', views.view_builds, name='view_builds'), 
 ]
